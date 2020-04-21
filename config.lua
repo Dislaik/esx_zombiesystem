c67ae3ca06176c393faa34558c9a942c4876aae9
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2020-04-21 05:02:50 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,30 @@
+Config = {}
+
+Config.SpawnZombie = 10
+Config.MinSpawnDistance = 80
+Config.MaxSpawnDistance = 100
+Config.DespawnDistance = 120
+
+Config.ZombieDropLoot = true
+Config.ProbabilityWeaponLoot = 3 -- 3%
+Config.ProbabilityMoneyLoot = 70 -- 3-40 = 37%
+--Weapons and Money are 40% and 60% is 'you not found nothing' alert, see the code to understand.
+Config.WeaponLoot = {
+	"WEAPON_ASSAULTRIFLE",
+	"WEAPON_PUMPSHOTGUN",
+	"WEAPON_PISTOL",
+	"WEAPON_MACHETE",
+	"WEAPON_CROWBAR",
+	"WEAPON_BAT",
+	"WEAPON_HATCHET"
+}
+
+Config.NoPeds = true
+Config.Blackout = true
+Config.MuteAmbience = true
+Config.NotHealthRecharge = true
+Config.SafeZone = true
+Config.SafeZoneRadioBlip = true
+Config.SafeZoneCoords = {x = -1118.1, y = 4926.28, z = 218.33, radio = 80.0}
+
+Config.Debug = false
