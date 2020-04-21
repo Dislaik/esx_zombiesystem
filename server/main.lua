e57ddab323109d6022ee5bd449990ecfe074c0a1
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2020-04-21 05:02:06 -0400

    Create main.lua

---
+++ server/main.lua
@@ -0,0 +1,37 @@
+ESX  = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+players = {}
+
+RegisterServerEvent("esx_zombiesystem:newplayer")
+AddEventHandler("esx_zombiesystem:newplayer", function(id)
+    players[source] = id
+    TriggerClientEvent("esx_zombiesystem:playerupdate", -1, players)
+end)
+
+AddEventHandler("playerDropped", function(reason)
+    players[source] = nil
+    TriggerClientEvent("esx_zombiesystem:clear", source)
+    TriggerClientEvent("esx_zombiesystem:playerupdate", -1, players)
+end)
+
+AddEventHandler("onResourceStop", function()
+	 TriggerClientEvent("esx_zombiesystem:clear", -1)
+end)
+
+RegisterServerEvent('esx_zombiesystem:moneyloot')
+AddEventHandler('esx_zombiesystem:moneyloot', function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+	local random = math.random(1, 20)
+    xPlayer.addMoney(random)
+    TriggerClientEvent("pNotify:SendNotification", xPlayer.source, {text = "You found $" .. random .. " dolars", type = "success", timeout = 2500, layout = "centerRight", queue = "right"})
+end)
+
+entitys = {}
+
+RegisterServerEvent("RegisterNewZombie")
+AddEventHandler("RegisterNewZombie", function(entity)
+	TriggerClientEvent("ZombieSync", -1, entity)
+	table.insert(entitys, entity)
+end)
