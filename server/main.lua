654216240a6e7befb4cbc970a1a13ea38922560e
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2020-04-21 05:07:49 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -6,26 +6,26 @@ players = {}
 
 RegisterServerEvent("esx_zombiesystem:newplayer")
 AddEventHandler("esx_zombiesystem:newplayer", function(id)
-    players[source] = id
-    TriggerClientEvent("esx_zombiesystem:playerupdate", -1, players)
+	players[source] = id
+	TriggerClientEvent("esx_zombiesystem:playerupdate", -1, players)
 end)
 
 AddEventHandler("playerDropped", function(reason)
-    players[source] = nil
-    TriggerClientEvent("esx_zombiesystem:clear", source)
-    TriggerClientEvent("esx_zombiesystem:playerupdate", -1, players)
+	players[source] = nil
+	TriggerClientEvent("esx_zombiesystem:clear", source)
+	TriggerClientEvent("esx_zombiesystem:playerupdate", -1, players)
 end)
 
 AddEventHandler("onResourceStop", function()
-	 TriggerClientEvent("esx_zombiesystem:clear", -1)
+	TriggerClientEvent("esx_zombiesystem:clear", -1)
 end)
 
 RegisterServerEvent('esx_zombiesystem:moneyloot')
 AddEventHandler('esx_zombiesystem:moneyloot', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
 	local random = math.random(1, 20)
-    xPlayer.addMoney(random)
-    TriggerClientEvent("pNotify:SendNotification", xPlayer.source, {text = "You found $" .. random .. " dolars", type = "success", timeout = 2500, layout = "centerRight", queue = "right"})
+	xPlayer.addMoney(random)
+	TriggerClientEvent("pNotify:SendNotification", xPlayer.source, {text = "You found $" .. random .. " dolars", type = "success", timeout = 2500, layout = "centerRight", queue = "right"})
 end)
 
 entitys = {}
