6713b99c6efd7fecb83d0a3d28c947c3254cd21d
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2020-04-25 08:18:39 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,9 +7,9 @@ Config.DespawnDistance = 120
 
 Config.ZombieDropLoot = true
 Config.ProbabilityWeaponLoot = 3 -- 3%
-Config.ProbabilityMoneyLoot = 40 -- 40-3 = 37%
-Config.ProbabilityItemLoot = 63 -- 63-40-3 = 20%
---Weapons, Money and Items are 60% and 40% is 'you not found nothing' alert, see the code to understand.
+Config.ProbabilityMoneyLoot = 33 -- 3-33 = 30%
+Config.ProbabilityItemLoot = 53 -- 53-43-3 = 7%
+--Weapons and Money are 40% and 60% is 'you not found nothing' alert, see the code to understand.
 Config.WeaponLoot = {
 	"WEAPON_ASSAULTRIFLE",
 	"WEAPON_PUMPSHOTGUN",
@@ -20,9 +20,51 @@ Config.WeaponLoot = {
 	"WEAPON_HATCHET"
 }
 Config.ItemLoot = {
-	'water',
-	'bread'
+	'bandage',
+	'medikit'
 }
+
+
+
+Config.ObjectDropLoot = true
+Config.ObjectsLoot = {
+    'prop_box_ammo03a',
+    'prop_bin_03a',
+    'prop_bin_02a',
+    'prop_bin_01a',
+    'prop_bin_04a',
+    'prop_bin_05a',
+    'prop_bin_06a',
+    'prop_bin_07a',
+    'prop_bin_07b',
+    'prop_bin_07c',
+    'prop_bin_07d',
+    'prop_bin_08open',
+    'prop_cs_bin_02',
+    'prop_box_wood01a',
+    'prop_box_wood03a',
+    'prop_box_wood07a',
+}
+
+Config.ProbabilityWeaponLootObject = 2 -- 2%
+Config.ProbabilityMoneyLootObject = 22 -- 22-2 20%
+Config.ProbabilityItemLootObject = 42 -- 52-22-2 18%
+--Weapons and Money are 40% and 60% is 'you not found nothing' alert, see the code to understand.
+Config.WeaponLootObject ={
+	"WEAPON_ASSAULTRIFLE",
+	"WEAPON_PUMPSHOTGUN",
+	"WEAPON_PISTOL",
+	"WEAPON_MACHETE",
+	"WEAPON_CROWBAR",
+	"WEAPON_BAT",
+	"WEAPON_HATCHET"
+}
+Config.ItemLootObject = {
+	'bread',
+	'water'
+}
+
+
 
 Config.NoPeds = true
 Config.Blackout = true
@@ -31,8 +73,7 @@ Config.NotHealthRecharge = true
 Config.SafeZone = true
 Config.SafeZoneRadioBlip = true
 Config.SafeZoneCoords = {
-	{x = -1118.1, y = 4926.28, z = 218.33, radio = 80.0},
-	{x = -1903.33, y = 4614.26, z = 57.01, radio = 20.0}
+	{x = 226.56, y = -1993.84, z = 19.55, radio = 30.0}
 }
 
 Config.Debug = false
