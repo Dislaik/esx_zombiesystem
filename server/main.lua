530e052f5875d6fec0dece0f5c70d8db5fb8c509
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2020-04-25 08:24:04 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -6,34 +6,34 @@ players = {}
 
 RegisterServerEvent("esx_zombiesystem:newplayer")
 AddEventHandler("esx_zombiesystem:newplayer", function(id)
-	players[source] = id
-	TriggerClientEvent("esx_zombiesystem:playerupdate", -1, players)
+    players[source] = id
+    TriggerClientEvent("esx_zombiesystem:playerupdate", -1, players)
 end)
 
 AddEventHandler("playerDropped", function(reason)
-	players[source] = nil
-	TriggerClientEvent("esx_zombiesystem:clear", source)
-	TriggerClientEvent("esx_zombiesystem:playerupdate", -1, players)
+    players[source] = nil
+    TriggerClientEvent("esx_zombiesystem:clear", source)
+    TriggerClientEvent("esx_zombiesystem:playerupdate", -1, players)
 end)
 
 AddEventHandler("onResourceStop", function()
-	TriggerClientEvent("esx_zombiesystem:clear", -1)
+	 TriggerClientEvent("esx_zombiesystem:clear", -1)
 end)
 
 RegisterServerEvent('esx_zombiesystem:moneyloot')
 AddEventHandler('esx_zombiesystem:moneyloot', function()
-	local xPlayer = ESX.GetPlayerFromId(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
 	local random = math.random(1, 20)
-	xPlayer.addMoney(random)
-	TriggerClientEvent("pNotify:SendNotification", xPlayer.source, {text = "You found $" .. random .. " dolars", type = "success", timeout = 2500, layout = "centerRight", queue = "right"})
+    xPlayer.addMoney(random)
+    TriggerClientEvent("esx:showNotification", xPlayer.source, ('You found ~g~$' .. random .. ' dolars'))
 end)
 
 RegisterServerEvent('esx_zombiesystem:itemloot')
 AddEventHandler('esx_zombiesystem:itemloot', function(item)
     local xPlayer = ESX.GetPlayerFromId(source)
-	local random = math.random(1, 5)
+	local random = math.random(1, 3)
     xPlayer.addInventoryItem(item, random)
-    TriggerClientEvent("pNotify:SendNotification", xPlayer.source, {text = "You found " .. random .. " " .. item, type = "success", timeout = 2500, layout = "centerRight", queue = "right"})
+    TriggerClientEvent("esx:showNotification", xPlayer.source, ('You found ~y~' .. random .. 'x ~b~' .. item))
 end)
 
 entitys = {}
