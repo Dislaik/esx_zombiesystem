1a315f14c5871ef8be0572885d1af67ae34ab05e
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2020-04-26 04:32:31 -0400

    Update loot.lua

--- client/loot.lua
+++ client/loot.lua
@@ -20,7 +20,7 @@ if Config.ObjectDropLoot then
 	        Wait(0)
 	        for k,v in pairs(Config.ObjectsLoot) do
 		        	local player = GetPlayerPed(-1)
-		        	local distanceobject = 1.2
+		        	local distanceobject = 2.2
 		            local obj = GetClosestObjectOfType(GetEntityCoords(player).x, GetEntityCoords(player).y, GetEntityCoords(player).z, distanceobject, v, false, true ,true)
 		            local distance = GetDistanceBetweenCoords(GetEntityCoords(player), GetEntityCoords(obj), true)
 		    
@@ -38,45 +38,28 @@ if Config.ObjectDropLoot then
 		                if not objects[obj] then
 
 		                    SetCurrentPedWeapon(GetPlayerPed(-1), 0xA2719263, true)
-							exports['mythic_progbar']:Progress({
-								name = "buscando",
-								duration = 6000,
-								label = 'BUSCANDO...',
-								useWhileDead = true,
-								canCancel = true,
-								controlDisables = {
-								disableMovement = true,
-								disableCarMovement = true,
-								disableMouse = false,
-								disableCombat = true,
-							},
-								animation = {
-								animDict = "anim@amb@clubhouse@tutorial@bkr_tut_ig3@",
-								anim = "machinic_loop_mechandplayer",
-								flags = 49,
-							},
-							}, function(cancelled)
-								if not cancelled then
-									randomChance = math.random(1, 100)
-									randomWeapon = Config.WeaponLootObject[math.random(1, #Config.WeaponLootObject)]
-									randomItem = Config.ItemLootObject[math.random(1, #Config.ItemLootObject)]
+		                    RequestAnimDict("anim@amb@clubhouse@tutorial@bkr_tut_ig3@")
+							while not HasAnimDictLoaded("anim@amb@clubhouse@tutorial@bkr_tut_ig3@") do
+								Citizen.Wait(10)
+							end
+							TaskPlayAnim(PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 2.0, 2.0, 4000, 30, 0, 0, 0, 0)
+							Citizen.Wait(4000)
+							randomChance = math.random(1, 100)
+							randomWeapon = Config.WeaponLootObject[math.random(1, #Config.WeaponLootObject)]
+							randomItem = Config.ItemLootObject[math.random(1, #Config.ItemLootObject)]
 
-									if randomChance > 0 and randomChance < Config.ProbabilityWeaponLootObject then
-										local randomAmmo = math.random(1, 30)
-										GiveWeaponToPed(GetPlayerPed(-1), randomWeapon, randomAmmo, true, false)
-										ESX.ShowNotification('You found ' .. randomWeapon)
-									elseif randomChance >= Config.ProbabilityWeaponLootObject and randomChance < Config.ProbabilityMoneyLootObject then
-										TriggerServerEvent('esx_zombiesystem:moneyloot')
-									elseif randomChance >= Config.ProbabilityMoneyLootObject and randomChance < Config.ProbabilityItemLootObject then
-										TriggerServerEvent('esx_zombiesystem:itemloot', randomItem)
-									elseif randomChance >= Config.ProbabilityItemLootObject and randomChance < 100 then
-										ESX.ShowNotification('You not found nothing')
-									end
-									objects[obj] = true
-								else
-									-- Do Something If Action Was Cancelled
-								end
-							end)
+							if randomChance > 0 and randomChance < Config.ProbabilityWeaponLootObject then
+								local randomAmmo = math.random(1, 30)
+								GiveWeaponToPed(GetPlayerPed(-1), randomWeapon, randomAmmo, true, false)
+								ESX.ShowNotification('You found ' .. randomWeapon)
+							elseif randomChance >= Config.ProbabilityWeaponLootObject and randomChance < Config.ProbabilityMoneyLootObject then
+								TriggerServerEvent('esx_zombiesystem:moneyloot')
+							elseif randomChance >= Config.ProbabilityMoneyLootObject and randomChance < Config.ProbabilityItemLootObject then
+								TriggerServerEvent('esx_zombiesystem:itemloot', randomItem)
+							elseif randomChance >= Config.ProbabilityItemLootObject and randomChance < 100 then
+								ESX.ShowNotification('You not found nothing')
+							end
+							objects[obj] = true
 		               	end
 		            end
 		        end
